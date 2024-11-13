ad11f6c56c3d0eac10f146c0e5a15fd1d4ed5956
vanessa <vanessacelestrini1@gmail.com>
2024-11-13 08:25:44 -0300

    Adicionando e modificando arquivos em Aula_html

---
+++ aula22/revisao.py
@@ -0,0 +1,26 @@
+def calcularDivisao(v1, v2):
+    resultado = v1 / v2
+    return resultado
+
+print ('Revisão de Python')
+
+print(325 * 5)
+
+total = 325 * 5
+
+print(total)
+
+valor = input('Digite seu nome')
+
+print('foi digitado',valor)
+
+for i in range(10):
+
+    print(f'foi digitado {valor}')
+
+print(calcularDivisao(5 , 1))   
+
+eduarda = [5 ,2 , 1 , 25 , 66]
+
+for elemento in eduarda:
+    print(elemento)
